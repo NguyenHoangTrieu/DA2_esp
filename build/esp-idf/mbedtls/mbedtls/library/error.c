cf4314a8b8624115ee03dc877796391fb23307c8
NguyenHoangTrieu <nguyentrieu.work@gmail.com>
2025-10-08 17:01:43 +0700

    Initialize

--- build/esp-idf/mbedtls/mbedtls/library/error.c
+++
@@ -1,1 +0,0 @@
-/home/trieunguyen0406/esp-idf/components/mbedtls/mbedtls/library/error.c
--- build/esp-idf/mbedtls/mbedtls/library/ssl_debug_helpers_generated.c
+++
@@ -1,1 +0,0 @@
-/home/trieunguyen0406/esp-idf/components/mbedtls/mbedtls/library/ssl_debug_helpers_generated.c
