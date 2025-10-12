cff8ed1fa7fca74d92d5cec9d9322692f202cffe
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-10-12 21:57:22 +0700

    Update mqtt_handler.c

--- mqtt_handler/mqtt_handler.c
+++ mqtt_handler/mqtt_handler.c
@@ -7,6 +7,8 @@
 #define BROKER_URI        "mqtt://demo.thingsboard.io:1883"
 #define DEVICE_TOKEN      "ZCOjw6KKw5j2EqYV2co6"
 #define PUBLISH_TOPIC     "v1/devices/me/telemetry"
+#define SUBSCRIBE_TOPIC   "v1/devices/me/rpc/request/+"
+#define ATRIBUTE_TOPIC    "v1/devices/me/attributes"
 #define PUBLISH_INTERVAL  1000  // ms
 
 static const char *TAG = "mqtt_handler";
@@ -32,6 +34,11 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
         case MQTT_EVENT_ERROR:
             ESP_LOGE(TAG, "MQTT_EVENT_ERROR");
             m_mqtt_connected = false;
+            break;
+        case case MQTT_EVENT_DATA:
+            ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+            ESP_LOGI(TAG, "TOPIC=%.*s", event->topic_len, event->topic);
+            ESP_LOGI(TAG, "DATA=%.*s", event->data_len, event->data);
             break;
         default:
             break;
@@ -76,6 +83,8 @@ void mqtt_handle_start(void)
     m_client = esp_mqtt_client_init(&mqtt_cfg);
     esp_mqtt_client_register_event(m_client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
     esp_mqtt_client_start(m_client);
+    esp_mqtt_client_subscribe(m_client, SUBSCRIBE_TOPIC, 1);
+    esp_mqtt_client_subscribe(m_client, ATRIBUTE_TOPIC, 1);
 
     // Start publish task and immediately suspend it
     xTaskCreate(mqtt_publish_task, "mqtt_publish_task", 4096, NULL, 5, &m_pub_task);
