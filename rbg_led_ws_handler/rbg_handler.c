3a923fbaa1303ebe68de7c61977793485ada168d
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-10-17 19:29:35 +0700

    Update

--- rbg_led_ws_handler/rbg_handler.c
+++ rbg_led_ws_handler/rbg_handler.c
@@ -97,7 +97,7 @@ void led_show_red(void) { show_led_color(255, 0, 0); }       // Red: error
 void led_show_white(void) { show_led_color(255, 255, 255); } // White: IDLE
 void led_off(void) { show_led_color(0, 0, 0); }
 void led_show_yellow(void) { show_led_color(255, 255, 0); } // Yellow
-void led_show_purple(void) { show_led_color(128, 0, 128); } // Purple
+void led_show_brown(void) { show_led_color(165, 42, 42); } // Brown
 
 void led_toggle_white(void) {
   static bool led_on = false;
