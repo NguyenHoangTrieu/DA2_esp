cff2848078ef5f01989a302ed966c9e424c87358
NguyenHoangTrieu <nguyentrieu.work@gmail.com>
2025-12-14 09:28:57 +0700

    Update

--- build.sh
+++ build.sh
@@ -10,9 +10,9 @@ source ~/esp-idf/export.sh
 idf.py build
 
 #4. copy firmware to flash directory
-cp -r build/DA2_esp.bin /mnt/c/embedded/esp_flash_folder/da2_esp
-cp -r build/bootloader/bootloader.bin /mnt/c/embedded/esp_flash_folder/da2_esp
-cp -r build/partition_table/partition-table.bin /mnt/c/embedded/esp_flash_folder/da2_esp
+cp -r build/DA2_esp.bin /mnt/c/embedded/DATN_Workspace/DA2_esp/config_app/dist
+cp -r build/bootloader/bootloader.bin /mnt/c/embedded/DATN_Workspace/DA2_esp/config_app/dist
+cp -r build/partition_table/partition-table.bin /mnt/c/embedded/DATN_Workspace/DA2_esp/config_app/dist
 
 cp -r build/DA2_esp.bin flash_file
 cp -r build/bootloader/bootloader.bin flash_file
